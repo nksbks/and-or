ed847773ebcea21e540acd561c1b9ea6fdf4e34f
nksbks <121257584+nksbks@users.noreply.github.com>
2022-12-25 07:37:08 +0200

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,39 @@
+input.onButtonPressed(Button.AB, function () {
+    if (input.soundLevel() >= 80) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (gate > 0) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+})
+input.onSound(DetectedSound.Loud, function () {
+    if (gate > 0) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (gate > 0) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    gate = gate * -1
+    if (gate > 0) {
+        basic.showString("OR")
+    } else {
+        basic.showString("AND")
+    }
+})
+let gate = 0
+gate = 1
+basic.showString("OR")
